172567623d162e7611cd0b694a1b9264c994e27d
Vitalii <vitalii.bondarenko.ua@gmail.com>
2021-04-01 22:28:31 +0300

    cleanup

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -6,7 +6,7 @@ export class Toolbar extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Toolbar',
-      listeners: ['click'],
+      listeners: [],
       ...options,
     });
   }
@@ -31,9 +31,5 @@ export class Toolbar extends ExcelComponent {
         <i class="material-icons">format_underline</i>
       </div>
     `;
-  }
-
-  onClick(event) {
-    console.log(event.target);
   }
 }
