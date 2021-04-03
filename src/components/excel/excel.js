d51c8124c5e9ba612a917628293968c2ca327c76
Vitalii <vitalii.bondarenko.ua@gmail.com>
2021-04-03 13:22:20 +0300

    create store

--- src/components/excel/excel.js
+++ src/components/excel/excel.js
@@ -2,16 +2,18 @@ import {$} from '../../core/dom';
 import {Emitter} from '../../core/Emitter';
 
 export class Excel {
-  constructor(selector, option) {
+  constructor(selector, options) {
     this.$el = $(selector);
-    this.components = option.components || [];
+    this.components = options.components || [];
     this.emitter = new Emitter();
+    this.store = options.store;
   }
 
   getRoot() {
     const $root = $.create('div', 'excel');
     const componentOptions = {
       emitter: this.emitter,
+      store: this.store,
     };
 
     this.components = this.components.map((Component) => {
--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ import {Header} from './components/header/Header';
 import {Toolbar} from './components/toolbar/Toolbar';
 import {Formula} from './components/formula/Formula';
 import {Table} from './components/table/Table';
+import {rootReducer} from './redux/rootReducer';
+import {createStore} from './core/createStore';
+
+const store = createStore(rootReducer, {
+  tableTitle: 'My Table',
+});
 
 const excel = new Excel('#app', {
   components: [Header, Toolbar, Formula, Table],
+  store,
 });
 
 excel.render();
